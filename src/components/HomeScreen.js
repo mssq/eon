6c40d4abc84fa6798979cee26caef7aae946977c
mssq <smiikasiren@gmail.com>
2017-11-15 14:45:48 +0200

    Added login form that has no other functionality than moving to home screen after pressing a button Added empty home screen to test navigation

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Navigation Screen!</Text>
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+  state = { email: '', password: '' };
+
+  renderButton() {    
+    const { navigate } = this.props.navigation;
+
+    return (
+      <Button
+        onPress={() => navigate('Home')}
+      >
+        Log in
+      </Button>
+    );
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input 
+            label="Email"
+            placeholder="john.doe@gmail.com"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+        <Input 
+            label="Password"
+            placeholder="password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            secureTextEntry
+        />
+        </CardSection>
+
+        <CardSection>
+          {this.renderButton()}
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
