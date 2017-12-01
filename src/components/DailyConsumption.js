8b1bfaf46daddadc76f71b0c330576b2e70425c4
mssq <smiikasiren@gmail.com>
2017-12-01 20:02:52 +0200

    Modified daily goal to be reusable component

---
+++ src/components/DailyConsumption.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+import userData from './data/userData.json';
+
+const DailyConsumption = () => {
+  const { goalStyle, numberStyle, goalTextStyle } = styles;
+  const { dailyConsumptionValue, consumptionUnit, goals, currentGoal } = userData;
+
+  return (
+    <CardSection style={goalStyle}>
+      <Text style={goalTextStyle}>DAILY GOAL</Text>
+      <Text style={numberStyle}>
+        {dailyConsumptionValue} {consumptionUnit} / {goals[currentGoal].targetValue} {goals[currentGoal].targetUnit}
+      </Text>
+    </CardSection>
+  )
+};
+
+const styles = {
+  goalStyle: {
+    height: 90,
+    width: 200,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(70, 130, 180, 1)',
+    borderRadius: 12
+  },
+  numberStyle: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 26,
+    marginTop: 0
+  },
+  goalTextStyle: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 5
+  }
+};
+
+export { DailyConsumption };
