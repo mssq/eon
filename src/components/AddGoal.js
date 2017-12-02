df254abc297c004cc6748ddc1e4341c9561c0ff6
mssq <smiikasiren@gmail.com>
2017-12-02 16:36:36 +0200

    Added the structure of the screen

--- src/components/AddGoal.js
+++ src/components/AddGoal.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import { CardSection, Card, Button } from './common';
+import { Text, Picker, Dimensions } from 'react-native';
+import { CardSection, Card, Button, Input } from './common';
 
 class HomeScreen extends Component {
+  state = { targetValue: '', targetUnit: '', targetTimespawn: '' };
 
   render() {
-    const { cardStyle, buttonStyle } = styles;
+    const { cardStyle, buttonStyle, pickerSectionStyle, pickerStyle, pickerTextStyle } = styles;
 
     return (
       <Card style={cardStyle}>
+
+        <CardSection>
+          <Input 
+            label="Target value:"
+            keyboardType='numeric'
+            value={this.state.targetValue}
+            onChangeText={targetValue => this.setState({ targetValue })}
+          />
+        </CardSection>
+
+        <CardSection style={pickerSectionStyle}>
+          <Text style={pickerTextStyle}>Target unit:</Text>
+          <Picker
+            style={pickerStyle}
+            selectedValue={this.state.targetUnit}
+            onValueChange={targetUnit => this.setState({ targetUnit })}
+          >
+            <Picker.Item label="kW" value="kw" />
+            <Picker.Item label="W" value="w" />
+          </Picker>
+        </CardSection>
+
+        <CardSection style={pickerSectionStyle}>
+          <Text style={pickerTextStyle}>Target timespawn:</Text>
+          <Picker
+            style={pickerStyle}
+            selectedValue={this.state.targetTimespawn}
+            onValueChange={targetTimespawn => this.setState({ targetTimespawn })}
+          >
+            <Picker.Item label="Daily" value="daily" />
+            <Picker.Item label="Monthly" value="monthly" />
+          </Picker>
+        </CardSection>
+
         <CardSection style={buttonStyle}>
           <Button>SET GOAL</Button>
         </CardSection>
@@ -28,6 +64,24 @@ const styles = {
     justifyContent: 'flex-start',
     flexDirection: 'row',
     position: 'relative'
+  },
+  pickerSectionStyle: {
+    width: Dimensions.get('window').width,
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    borderBottomWidth: 1,
+    borderColor: '#ddd'
+  },
+  pickerStyle: {
+    width: 230,
+    height: 40
+  },
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 25,
+    flex: 1
   }
 };
 
