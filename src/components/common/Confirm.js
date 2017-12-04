1e3bef45ed51fe9c07dd92f85beca6a89cf3c30e
mssq <smiikasiren@gmail.com>
2017-12-04 11:11:40 +0200

    Reusable component for popup modal

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = ({ children, visible, onAccept }) => {
+  const { cardSectionStyle, textStyle, containerStyle } = styles;
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={onAccept}>Ok</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    height: 50
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+};
+
+export { Confirm };
